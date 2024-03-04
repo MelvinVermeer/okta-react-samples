50f281df0a2b4cb5da6154fbae180e50d4733261
Melvin Vermeer <melvin.vermeer@conclusion.nl>
2024-03-04 15:35:21 +0100

    add query client to the context

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import { Security, useOktaAuth } from "@okta/okta-react";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const oktaAuth = new OktaAuth({
   clientId: import.meta.env.VITE_CLIENT_ID,
   issuer: import.meta.env.VITE_ISSUER,
   redirectUri: window.location.origin + "/callback",
-  pkce: false,
+  pkce: true,
+  scopes: ["openid", "profile", "email", "role:app", "organization:read"],
 });
+
+const queryClient = new QueryClient();
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -19,10 +23,12 @@ declare module "@tanstack/react-router" {
 }
 
 const router = createRouter({
-  routeTree,
   context: {
+    queryClient,
     auth: undefined!, // This will be set after we wrap the app in an AuthProvider
   },
+  defaultPreload: "intent", // hovering a link will trigger a preload of the data
+  routeTree,
 });
 
 function InnerApp() {
@@ -41,6 +47,7 @@ function App() {
         // the provided restoreOriginalUri prop from the Security component.
         router.update({
           context: {
+            queryClient,
             auth: {
               oktaAuth,
               authState: oktaAuth.authStateManager.getAuthState(),
@@ -53,7 +60,9 @@ function App() {
         });
       }}
     >
-      <InnerApp />
+      <QueryClientProvider client={queryClient}>
+        <InnerApp />
+      </QueryClientProvider>
     </Security>
   );
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,9 @@
 import { IOktaContext } from "@okta/okta-react/bundles/types/OktaContext";
+import { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 
 interface MyRouterContext {
+  queryClient: QueryClient;
   auth: IOktaContext;
 }
 
